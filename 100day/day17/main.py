8bc0ec27e8760424f5e9f9e821981900789de26e
LukeShin3022 <jwshin3022@gmail.com>
2023-07-20 17:09:57 -0700

    Day 17-pros of OOP start

---
+++ 100day/day17/main.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.usernamae = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        user.following += 1
+
+user_1 = User("001", "Luke")
+user_1 = User("002", "Shin")
+
+print(user_1.followers)
