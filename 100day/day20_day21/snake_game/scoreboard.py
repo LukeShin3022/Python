c446fa31062bb345d1530117b01790ea122f4d9b
LukeShin3022 <jwshin3022@gmail.com>
2023-08-08 16:06:22 -0700

    Making Snake Game: Done making the snake game

--- 100day/day20_day21/snake_game/scoreboard.py
+++ 100day/day20_day21/snake_game/scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
 
@@ -8,8 +10,18 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0,270)
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.goto(0,260)
+        self.update_scoreboard()
         self.hideturtle()
 
-    def
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"Game Over", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
