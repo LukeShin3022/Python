4dab72b755a54b4635ff2261d3cfe983fc26c82e
LukeShin3022 <jwshin3022@gmail.com>
2023-08-08 15:24:43 -0700

    Making Snake Game: create ScoreBoasd class

--- 100day/day20_day21/snake_game/main.py
+++ 100day/day20_day21/snake_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,5 +25,10 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
+
 
 screen.exitonclick()
