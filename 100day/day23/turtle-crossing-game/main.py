f985262a21530b58d2641486f4207c4c8cb9285b
LukeShin3022 <jwshin3022@gmail.com>
2023-08-18 17:25:27 -0700

    Turtle Corssing Game: Setting the player and making cars

--- 100day/day23/turtle-crossing-game/main.py
+++ 100day/day23/turtle-crossing-game/main.py
@@ -8,7 +8,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager
+
+screen.listen()
+screen.onkey(player.go_up, "UP")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
